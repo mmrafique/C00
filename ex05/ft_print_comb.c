874de43945f0827763ce33f4585ab3fdfb344823
Muhammad Anas <anasabid607@gmail.com>
2025-08-14 00:15:22 +0200

    Subiendo otro

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -17,11 +17,37 @@ void	ft_iswrite(char a, char b, char c)
 	write(1, &a, 1);
 	write(1, &b, 1);
 	write(1, &c, 1);
-	if (a < 7)
-	
+	if (a != '7')
+		write(1, ", ", 2);
 }
 
 void	ft_print_comb(void)
 {
+	char	x;
+	char	y;
+	char	z;
 
+	x = '0';
+	while (x <= '7')
+	{
+		y = x + 1;
+		while (y <= '8')
+		{
+			z = y + 1;
+			while (z <= '9')
+			{
+				ft_iswrite(x, y, z);
+				z++;
+			}
+			y++;
+		}
+		x++;
+	}
 }
+
+/*int	main(void)
+{
+	ft_print_comb();
+	write(1, "\n", 1);
+	return (0);
+}*/
