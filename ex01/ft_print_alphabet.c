4b44c93e7762248b52d7565d2c61a66a3368bf30
Muhammad Anas Majeed Rafique <muhmajee@car6s2.42barcelona.com>
2025-08-13 21:20:42 +0200

    Subiendo todo

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhmajee <muhmajee@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/12 13:52:58 by muhmajee          #+#    #+#             */
+/*   Updated: 2025/08/12 13:53:06 by muhmajee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	int	i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		write(1, &i, 1);
+		i++;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}*/
---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhmajee <muhmajee@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/12 13:53:26 by muhmajee          #+#    #+#             */
+/*   Updated: 2025/08/12 14:08:23 by muhmajee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	int	i;
+
+	i = 'z';
+	while (i >= 'a')
+	{
+		write(1, &i, 1);
+		i--;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}*/
