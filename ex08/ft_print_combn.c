c21d6a8c3a23594f528bf0eb8e62e52887202a35
Muhammad Anas <anasabid607@gmail.com>
2025-08-14 23:19:01 +0200

    asdasd

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -1,42 +1,29 @@
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putnbr(int n)
-{
-	char	c;
-
-	if (n >= 10)
-		ft_putnbr(n / 10);
-	c = '0' + n % 10;
-	write(1, &c, 1);
-}
-
 void	ft_print_combn(int n)
 {
-	int	x;
-	int	y;
-	int	z;
+	int	a[10];
+	int	i;
 
-	x = 0;
-	y = 0;
-	z = 0;
-	while (m < n * n)
+	if (n <= 0 || n >= 10)
+		return ;
+	i = -1;
+	while (++i < n)
+		a[i] = i;
+	while (1)
 	{
-		while (x <= 9 && y <= 9 && z < n)
-		{
-			if (x != y)
-			{
-				write(1, &x, 1);
-			}
-			x++;
-			z++;
-		}
-	}
-}
+		i = -1;
+		while (++i < n)
+			write(1, &"0123456789"[a[i]], 1);
+		if (a[0] == 10 - n)
+			break ;
+		write(1, ", ", 2);
+		i = n;
+		while (i-- && a[i] == 9 - (n - 1 - i))
+			;
+		a[i]++;
+		while (++i < n)
+			a[i] = a[i - 1] + 1;
 
 int	main(void)
 {
