987c58a5f8577e4d61f8076ac739a6e41b8e0e63
Muhammad Anas Majeed Rafique <muhmajee@cbr1s3.42barcelona.com>
2025-08-15 14:04:01 +0200

    acceso

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -50,9 +50,9 @@ void	ft_print_comb2(void)
 	}
 }
 
-int	main(void)
+/*int	main(void)
 {
 	ft_print_comb2();
 	write(1, "\n", 1);
 	return (0);
-}
+}*/
--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -32,11 +32,11 @@ void	ft_putnbr(int nb)
 	write(1, &c, 1);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	ft_putnbr(-10);
 	write(1, "\n", 1);
 	ft_putnbr(10);
 	write(1, "\n", 1);
 	return (0);
-}
+}*/
